d309e3f41bbd864508354c94a68080b4c59b89ce
Nithin <nithinnitty56@gmail.com>
2026-02-18 17:08:43 +0530

    added Navigation for buy and sart sip

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabs from './src/navigation/BottomTabs';
+import StackNavigator from './src/navigation/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <BottomTabs />
+       <StackNavigator />
     </NavigationContainer>
   );
 };
